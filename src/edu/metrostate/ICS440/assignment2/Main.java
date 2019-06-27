fe571c5160db81629f249e3c673ff5cc8c3a6de5
Shannon Fisher <davine_chi@hotmail.com>
2019-06-27 10:58:50 -0500

    Add comment to Javadoc

--- src/edu/metrostate/ICS440/assignment2/Main.java
+++ src/edu/metrostate/ICS440/assignment2/Main.java
@@ -4,6 +4,11 @@ package edu.metrostate.ICS440.assignment2;
  * Main class for program entry.
  * <p>
  * 
+ * This application assumes that the directory "ghcnd_hcn" is on the classpath (or exists at the Java project's
+ * root), and that this directory contains the "ghcnd-stations.txt" file as well as a series of "*.dly" weather
+ * data files.
+ * <p>
+ * 
  * @author Shannon L. Fisher
  * <p>
  * Begin Date:	2019.06.07
