ffb63fd36e6177d5c2b3f404dff109a29b69d604
Shannon Fisher <davine_chi@hotmail.com>
2019-07-02 10:14:59 -0500

    Add Javadoc

--- src/edu/metrostate/ICS462/assignment6/Requests.java
+++ src/edu/metrostate/ICS462/assignment6/Requests.java
@@ -2,26 +2,60 @@ package edu.metrostate.ICS462.assignment6;
 
 import java.util.Vector;
 
+/****************************************************************************************************************
+ * This class handles add and get requests for cylinders and maintains a collection of cylinders that have
+ * generated access requests.
+ * <p>
+ * 
+ * @author Shannon L. Fisher
+ * <p>
+ * Begin Date:	2019.07.01
+ * <p>
+ * Due Date:	2019.07.09
+ */
 public class Requests {
 
-	private Vector<Integer> cylinderList = new Vector<Integer>();
+	private Vector<Integer> cylinderList;
 	
+	/************************************************************************************************************
+	 * Constructor for a new Requests object.
+	 * <p>
+	 * 
+	 * @postcondition
+	 *   A new Requests object has been created.
+	 */
 	public Requests() {
 		
-		
+		this.cylinderList = new Vector<Integer>();
 	}
 	
+	/************************************************************************************************************
+	 * Modifier method to add a cylinder. This activity represents a generated request for a cylinder.
+	 * <p>
+	 * 
+	 * @param cylinder
+	 *   the requeseted cylinder which is added to this Requests object
+	 */
 	public synchronized void add(Integer cylinder) {
 		
-		// TODO: Implementation
 		cylinderList.add(cylinder);
+		
 		// After adding a cylinder, notify all waiting threads.
 		notifyAll();
 	}
 	
+	/************************************************************************************************************
+	 * Accessor method to return a list of requests that have been added but not yet retrieved.
+	 * <p>
+	 * 
+	 * @param waitIfEmpty
+	 *   if true and there are no requests, the caller waits - if false OR there are requests,
+	 *   the caller does not wait
+	 * @return
+	 *   A list of requests that have been added but not retrieved yet.
+	 */
 	public synchronized Vector<Integer> get(boolean waitIfEmpty) {
 		
-		// TODO: Implementation
 		while (waitIfEmpty) {}  // wait...
 		
 		Vector<Integer> result = cylinderList;
@@ -29,5 +63,30 @@ public class Requests {
 		cylinderList.clear();
 		
 		return result;
+	}
+	
+	/************************************************************************************************************
+	 * Main method from where program execution begins. Used here for testing and debugging.
+	 * <p>
+	 * 
+	 * @param args
+	 *   parameter not used
+	 * 
+	 * @postcondition
+	 *   Program execution has transpired.
+	 */
+	public static void main(String[] args) {
+		
+		Requests req = new Requests();
+		
+		int iterations = 100;
+		
+		for (int index = 0; index < iterations; index++) {
+			
+			int candidate = ((int)(Math.random() * 10000)) % 5;
+			Integer integer = new Integer(candidate);
+			
+			req.add(integer);
+		}
 	}
 }
