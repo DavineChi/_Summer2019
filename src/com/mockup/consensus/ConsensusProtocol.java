e6ef3dd495f90a99b9fc86f740d9cca76733c5a3
Shannon Fisher <davine_chi@hotmail.com>
2019-07-18 10:24:08 -0500

    Provide push and pop operations

--- src/com/mockup/consensus/ConsensusProtocol.java
+++ src/com/mockup/consensus/ConsensusProtocol.java
@@ -2,6 +2,11 @@ package com.mockup.consensus;
 
 public abstract class ConsensusProtocol<T> implements Consensus<T> {
 	protected T[] proposed = (T[])new Object[2];
+	/**
+	 * A shared array of proposed input values.
+	 * @param value
+	 *   the proposed value to store
+	 */
 	public void propose(T value) {
 		proposed[(int)Thread.currentThread().getId()] = value;
 	}
--- src/com/mockup/consensus/StackConsensus.java
+++ src/com/mockup/consensus/StackConsensus.java
@@ -3,23 +3,25 @@ package com.mockup.consensus;
 import java.util.Stack;
 
 public class StackConsensus<T> extends ConsensusProtocol<T> {
-	private static final int WIN = 0;   // first thread
-	private static final int LOSE = 1;  // second thread
-	private Stack<Integer> stack;
+	private Stack<T> stack;
 	public StackConsensus() {
-		stack = new Stack<Integer>();
-		stack.push(LOSE);
-		stack.push(WIN);
+		stack = new Stack<T>();
 	}
 	@Override
 	public T decide(T value) {  // determine which thread went first
 		propose(value);
-		int status = stack.pop();
+		T item = stack.pop();
 		int i = (int)Thread.currentThread().getId();
-		if (status == WIN) {
+		if (item == value) {
 			return proposed[i];
 		} else {
 			return proposed[1 - i];
 		}
+	}
+	public void push(T x) {
+		stack.push(x);
+	}
+	public T pop() {
+		return decide(stack.pop());
 	}
 }
