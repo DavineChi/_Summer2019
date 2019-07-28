34955ef839869500f78c9f70269aa3bbfa4d17ec
Shannon Fisher <davine_chi@hotmail.com>
2019-07-27 23:27:17 -0500

    ICS440 updates

--- src/edu/metrostate/ICS440/assignment4/FloydWarshall.java
+++ src/edu/metrostate/ICS440/assignment4/FloydWarshall.java
@@ -2,96 +2,160 @@ package edu.metrostate.ICS440.assignment4;
 
 import java.util.Random;
 
+/****************************************************************************************************************
+ * Main class for program entry.
+ * <p>
+ * 
+ * [Application Description]
+ * 
+ * <p>
+ * Begin Date:	2019.07.25
+ * <p>
+ * Due Date:	2019.08.08
+ */
 public class FloydWarshall {
+	
 	private static final int INFINITY = Integer.MAX_VALUE; // Infinity
-	private static final int dim = 5000;
+	private static final int DIMENSION = 500;  // TODO: original value = 5000
+	
 	private static double fill = 0.3;
 	private static int maxDistance = 100;
-	private static int adjacencyMatrix[][] = new int[dim][dim];
-	private static int d[][] = new int[dim][dim];
-	/*
-	 * Generate a randomized matrix to use for the algorithm.
-	 */
+	private static int adjacencyMatrix[][] = new int[DIMENSION][DIMENSION];
+	private static int d[][] = new int[DIMENSION][DIMENSION];
+	
+	// **********************************************************************************************************
+	// Helper method to generate a randomized matrix to use for the algorithm.
+	// 
 	private static void generateMatrix() {
+		
 		Random random = new Random();
-		for (int i = 0; i < dim; i++)
-		{
-			for (int j = 0; j < dim; j++)
-			{
-				if (i != j)
+		
+		for (int i = 0; i < DIMENSION; i++) {
+			
+			for (int j = 0; j < DIMENSION; j++) {
+				
+				if (i != j) {
+					
 					adjacencyMatrix[i][j] = INFINITY;
+				}
 			}
 		}
-		for (int i = 0; i < dim * dim * fill; i++)
-		{
-			adjacencyMatrix[random.nextInt(dim)][random.nextInt(dim)] =
-					random.nextInt(maxDistance + 1);
+		
+		for (int i = 0; i < DIMENSION * DIMENSION * fill; i++) {
+			
+			adjacencyMatrix[random.nextInt(DIMENSION)][random.nextInt(DIMENSION)] = random.nextInt(maxDistance + 1);
 		}
 	}
-	/*
-	 * Execute Floyd Warshall on adjacencyMatrix.
-	 */
+	
+	// **********************************************************************************************************
+	// Helper method to execute Floyd Warshall on adjacencyMatrix.
+	// 
 	private static void execute() {
-		for (int i = 0; i < dim; i++) {
-			for (int j = 0; j < dim; j++)
-			{
+		
+		for (int i = 0; i < DIMENSION; i++) {
+			
+			for (int j = 0; j < DIMENSION; j++) {
+				
 				d[i][j] = adjacencyMatrix[i][j];
-				if (i == j)
-				{
+				
+				if (i == j) {
+					
 					d[i][j] = 0;
 				}
 			}
 		}
-		for (int k = 0; k < dim; k++) {
-			for (int i = 0; i < dim; i++) {
-				for (int j = 0; j < dim; j++) {
+		
+		for (int k = 0; k < DIMENSION; k++) {
+			
+			for (int i = 0; i < DIMENSION; i++) {
+				
+				for (int j = 0; j < DIMENSION; j++) {
+					
 					if (d[i][k] == INFINITY || d[k][j] == INFINITY) {
+						
 						continue;
-					} else if (d[i][j] > d[i][k] + d[k][j]) {
+					}
+					
+					else if (d[i][j] > d[i][k] + d[k][j]) {
+						
 						d[i][j] = d[i][k] + d[k][j];
 					}
 				}
 			}
+			
 			//System.out.println("pass " + (k + 1) + "/" + dim);
 		}
 	}
-	/*
-	 * Print matrix[dim][dim]
-	 */
+	
+	// **********************************************************************************************************
+	// Helper method to print matrix[dim][dim]
+	// 
 	private static void print(int matrix[][]) {
-		for (int i = 0; i < dim; i++) {
-			for (int j = 0; j < dim; j++) {
+		
+		for (int i = 0; i < DIMENSION; i++) {
+			
+			for (int j = 0; j < DIMENSION; j++) {
+				
 				if (matrix[i][j] == INFINITY) {
+					
 					System.out.print("I" + " ");
-				} else {
+				}
+				
+				else {
+					
 					System.out.print(matrix[i][j] + " ");
 				}
 			}
+			
 			System.out.println();
 		}
 	}
-	/*
-	 * Compare two matrices, matrix1[dim][dim] and matrix2[dim][dim] and
-	 * print whether they are equivalent.
-	 */
+	
+	// **********************************************************************************************************
+	// Helper method to compare two matrices, matrix1[dim][dim] and matrix2[dim][dim]
+	// and print whether they are equivalent.
+	// 
 	private static void compare(int matrix1[][], int matrix2[][]) {
-		for (int i = 0; i < dim; i++) {
-			for (int j = 0; j < dim; j++) {
-				if (matrix1[i][j] != matrix2[i][j])
-				{
-					System.out.println("Comparison failed");
+		
+		for (int i = 0; i < DIMENSION; i++) {
+			
+			for (int j = 0; j < DIMENSION; j++) {
+				
+				if (matrix1[i][j] != matrix2[i][j]) {
+					
+					System.out.println("Comparison failed.");
 				}
 			}
 		}
-		System.out.println("Comparison succeeded");
+		
+		System.out.println("Comparison succeeded.");
 	}
+	
+	/************************************************************************************************************
+	 * Main method from where program execution begins.
+	 * <p>
+	 * 
+	 * @param args
+	 *   parameter not used
+	 * 
+	 * @postcondition
+	 *   The application has been executed.
+	 */
 	public static void main(String[] args) {
-		long start, end;
+		
+		long start;
+		long end;
+		
 		generateMatrix();
+		
 		start = System.nanoTime();
+		
 		execute();
+		
 		end = System.nanoTime();
+		
 		System.out.println("time consumed: " + (double)(end - start) / 1000000000);
+		
 		compare(d, d);
 	}
 }
