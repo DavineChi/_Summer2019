31b2032e6dbb2a67289710bbeda902d76031bdea
Shannon Fisher <davine_chi@hotmail.com>
2019-07-30 09:12:41 -0500

    Add ICS462 programming assignment part 4 content ##################################################

---
+++ src/edu/metrostate/ICS462/assignment6/part4/FirstComeFirstServed.java
@@ -0,0 +1,25 @@
+package edu.metrostate.ICS462.assignment6.part4;
+
+public class FirstComeFirstServed extends Scheduler {
+
+	public FirstComeFirstServed(Requests requests, int numberOfRequests) {
+		
+		super(requests, numberOfRequests);
+	}
+
+	@Override
+	public void initialize() {
+		
+	}
+
+	@Override
+	public void processNextRequest() {
+		
+	}
+
+	@Override
+	public String getAlgorithmName() {
+		
+		return null;
+	}
+}
